2dbb46473538d2156f735ad47590895c5c46cdcc
sumitgiri <sumit.giri@indusnet.co.in>
2024-05-21 18:29:29 +0530

    update clode background

--- src/Component/MyGlobe/MyGlobe.js
+++ src/Component/MyGlobe/MyGlobe.js
@@ -4,7 +4,7 @@ import { INDIA_DATA } from "./Data/countriesData";
 // import HEX_DATA from "./Data/countriesHexData.json";
 import Globe from "react-globe.gl";
 import * as THREE from "//unpkg.com/three/build/three.module.js";
-
+import cloudUrl from "./clouds.png";
 // const getRandomCountry = () => {
 //   return COUNTRIES_DATA[Math.floor(Math.random() * COUNTRIES_DATA.length)];
 // };
@@ -31,7 +31,7 @@ export default function CustomGlobe() {
     const CLOUDS_ALT = 0.004;
     const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
 
-    new THREE.TextureLoader().load(CLOUDS_IMG_URL, (cloudsTexture) => {
+    new THREE.TextureLoader().load(cloudUrl, (cloudsTexture) => {
       const clouds = new THREE.Mesh(
         new THREE.SphereGeometry(globe.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
         new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
@@ -89,18 +89,23 @@ export default function CustomGlobe() {
 
   return (
     <Globe
+      // ref={globeEl}
+      // backgroundColor="rgba(150,0,0,0)"
+      // labelsData={[selectedCountry]}
+      // labelText={"label"}
+      // labelSize={1.6}
+      // labelColor={() => "rgba(255, 165, 0, 0.75)"}
+      // labelDotRadius={0.4}
+      // labelAltitude={0.05}
+      // hexPolygonsData={hex.features}
+      // hexPolygonResolution={3} //values higher than 3 makes it buggy
+      // hexPolygonMargin={0.62}
+      // hexPolygonColor={useCallback(() => "#1b66b1", [])}
+      // globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+      // bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+
       ref={globeEl}
-      backgroundColor="rgba(150,0,0,0)"
-      labelsData={[selectedCountry]}
-      labelText={"label"}
-      labelSize={1.6}
-      labelColor={() => "rgba(255, 165, 0, 0.75)"}
-      labelDotRadius={0.4}
-      labelAltitude={0.05}
-      hexPolygonsData={hex.features}
-      hexPolygonResolution={3} //values higher than 3 makes it buggy
-      hexPolygonMargin={0.62}
-      hexPolygonColor={useCallback(() => "#1b66b1", [])}
+      animateIn={false}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
     />
